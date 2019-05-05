d623bbdbf0dacdc3f85438ec6bc48c9abc17f68b
robertoeugenio1978@gmail.com <robertoeugenio>
2019-05-05 15:39:10 -0300

    alterando arquivo para comtemplar a rota

--- src/app/desenhos/desenhos.module.ts
+++ src/app/desenhos/desenhos.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { DesenhoListComponent } from './desenho-list/desenho-list.component';
+import { desenhosRouting } from './desenhos.routing';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   imports: [
-    CommonModule
+    CommonModule, desenhosRouting, FormsModule
   ],
   declarations: [DesenhoListComponent]
 })
