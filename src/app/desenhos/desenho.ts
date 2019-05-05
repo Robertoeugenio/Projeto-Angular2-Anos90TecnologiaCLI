b731630f4cec8f1573fd260946b6fbce9256e743
robertoeugenio1978@gmail.com <robertoeugenio>
2019-05-05 15:08:23 -0300

    criando desenho list e a classe desenho

---
+++ src/app/desenhos/desenho.ts
@@ -0,0 +1,10 @@
+export class Desenho {
+
+     codigo: number;
+     nomeDesenho: string;
+     categoria: string;
+     exibicao: string;
+     melhorEpisodio; string;
+     notas: string;
+     piorDesenho: string;
+}
