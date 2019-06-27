6fe21bcf30565579eeab9995133cb9596872990f
robertoeugenio1978@gmail.com <robertoeugenio>
2019-06-26 21:24:23 -0300

    modificando app.js

--- nodeBackEnd/app.js
+++ nodeBackEnd/app.js
@@ -1,16 +1,19 @@
-const express = require ('express');
+const express = require('express'); // atualizado
 
-const PORT =3000;
+
+const PORT = 3000; 
+
 
 const app = express();
 
-app.get("/clientes", (req, res)=> {
+
+app.get("/clientes", (req, res) => {       //de acordo com slide
     res.json(
         [
-            {'codigo': 1, 'nome': 'unilever'},
-            {'codigo': 2, 'nome': 'bombril'}
+            { 'codigo': 1, 'nome': 'Roberto' },        //passo a passo do slide
+            { 'codigo': 2, 'nome': 'Eugenio' }
         ]
     )
-}); 
+});
 
-app.listen(PORT, () => console.log("escutando na porta" + PORT));
+app.listen(PORT, () => console.log("ESCUTANDO NA PORTA" + PORT));
