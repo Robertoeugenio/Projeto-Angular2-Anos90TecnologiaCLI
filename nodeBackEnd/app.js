a93d950379a72ae4af231890d9fd1b7d03d19fa9
robertoeugenio1978@gmail.com <robertoeugenio>
2019-06-27 19:37:48 -0300

    migrando os endpoints RES

--- nodeBackEnd/app.js
+++ nodeBackEnd/app.js
@@ -1,18 +1,24 @@
 const express = require('express'); // atualizado
-
 const PORT = 4000; 
-
 const app = express();
 
 app.set("json spaces",4);
 
-app.get("/clientes", (req, res) => {       //de acordo com slide
+const index = require('.routes/index');
+const clientes = require('.routes/clientes');
+
+app.use('/', index);
+app.use('/clientes', clientes);
+
+
+
+/*app.get("/clientes", (req, res) => {       //de acordo com slide
     res.json(
         [
             { 'codigo': 1, 'nome': 'Roberto' },        //passo a passo do slide
             { 'codigo': 2, 'nome': 'Eugenio' }
         ]
     )
-});
+});*/
 
 app.listen(PORT, () => console.log("escutando na porta" + PORT));
